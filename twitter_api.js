7a53f679d425cec36803de985e6b5aeca03367a8
liamgorey11 <57719630+liamgorey11@users.noreply.github.com>
2023-02-06 18:47:58 -0500

    Switched from using Promise.then to await Promise
    Updated /searchReddit endpoint to async,

--- twitter_api.js
+++ twitter_api.js
@@ -44,103 +44,100 @@ const start = async () => {
 start();
 
 
-function test() {
-  const snoo = new SnooShift({ // get proper values here 
-    userAgent:  process.env.Reddit_userAgent,
-    clientId: process.env.Reddit_clientId,
-    clientSecret: process.env.Reddit_clientSecret,
-    //refreshToken: process.env.Reddit_refreshToken,
-    username: process.env.Reddit_username,
-    password: process.env.Reddit_password 
-  }); 
+app.get('/searchReddit', async (req, res) => {
+  const searchTerm = req.query.searchTerm;
+  console.log(req.query.searchTerm);
+  const snoo = new SnooShift(); 
   const searchParams = {
-    q: "\"Obama\"",
-    size: 100,
+    q: searchTerm,
+    size: 30,
     order: 'asc',
     sort: 'created_utc'
-    };
-   snoo.searchComments(searchParams)
-    .then((comments) => {
-      let commentBody = comments.map(comment => comment.body + "\n");
-      let bodyText = commentBody.join("");
-      let he = require('he');
-      let fixedText = he.decode(bodyText);
-      fs.writeFileSync('reddit.txt', fixedText);
-      //console.log(fixedText);
-      const naturalLanguageUnderstanding = new NaturalLanguageUnderstandingV1({
-        version: '2022-04-07',
-        authenticator: new IamAuthenticator({
-          apikey: process.env.Watson_apikey,
-        }),
-        serviceUrl: process.env.Watson_serviceUrl,
-      });
-
-      const analyzeParams = {
-        'text': fixedText,
-        'features': {
-          'entities': {
-            'emotion': true,
-            'sentiment': true,
-          },
-          'keywords': {
-            'emotion': true,
-            'sentiment': true,
-          },
-        },
-      };
-      naturalLanguageUnderstanding.analyze(analyzeParams)
-        .then(analysisResults => {
-          //console.log(JSON.stringify(analysisResults, null, 2));
-          const outputString = JSON.stringify(analysisResults, null, 2);
-          fs.writeFileSync('results.json', outputString);
-          let sentimentResults = analysisResults.result.keywords;
-          var sadness=0;var joy=0;var fear=0;var disgust=0;var anger=0;
-          //sadness
-          for(let i = 0; i < sentimentResults.length; i++) {
-            sadness+=sentimentResults[i].emotion.sadness;
-          }
-          sadness/=sentimentResults.length;
-          sadness = sadness.toFixed(2);
-          //joy
-          for(let i = 0; i < sentimentResults.length; i++) {
-            joy+=sentimentResults[i].emotion.joy;
-          }
-          joy/=sentimentResults.length;
-          joy = joy.toFixed(2);
-          //fear
-          for(let i = 0; i < sentimentResults.length; i++) {
-            fear+=sentimentResults[i].emotion.fear;
-          }
-          fear/=sentimentResults.length;
-          fear = fear.toFixed(2);
-          //disgust
-          for(let i = 0; i < sentimentResults.length; i++) {
-            disgust+=sentimentResults[i].emotion.disgust;
-          }
-          disgust/=sentimentResults.length;
-          disgust = disgust.toFixed(2);
-          //anger
-          for(let i = 0; i < sentimentResults.length; i++) {
-            anger+=sentimentResults[i].emotion.anger;
-          }
-          anger/=sentimentResults.length;
-          anger = anger.toFixed(2);
-          var neutral = 1 
-          neutral = neutral - sadness - joy - fear - disgust - anger;
-          console.log("RESULTS");
-          console.log("sadness: "+sadness);
-          console.log("joy: "+joy);
-          console.log("fear: "+fear);
-          console.log("disgust: "+disgust);
-          console.log("anger: "+anger);
-          console.log("neutral: "+neutral);
-        })
-        .catch(err => {
-          console.log('error:', err);
-        });
+  };
+  try{
+    const comments = await snoo.searchComments(searchParams);
+    let commentBody = comments.map(comment => comment.body + "\n");
+    let bodyText = commentBody.join("");
+    let he = require('he');
+    let fixedText = he.decode(bodyText);
+    //fs.writeFileSync('reddit.txt', fixedText);
+    //console.log(fixedText);
+    const naturalLanguageUnderstanding = new NaturalLanguageUnderstandingV1({
+      version: '2022-04-07',
+      authenticator: new IamAuthenticator({
+        apikey: process.env.Watson_apikey,
+      }),
+      serviceUrl: process.env.Watson_serviceUrl,
     });
-}  
-test();
+    
+    const analyzeParams = {
+      'text': fixedText,
+      'features': {
+        'entities': {
+          'emotion': true,
+          'sentiment': true,
+        },
+        'keywords': {
+          'emotion': true,
+          'sentiment': true,
+        },
+      },
+    };
+    const analysisResults = await naturalLanguageUnderstanding.analyze(analyzeParams);
+    //console.log(JSON.stringify(analysisResults, null, 2));
+    //const outputString = JSON.stringify(analysisResults, null, 2);
+    //fs.writeFileSync('results.json', outputString);
+     let sentimentResults = analysisResults.result.keywords;
+    // var sadness=0;
+     var joy=0;
+    // var fear=0;S
+    // var disgust=0;
+    // var anger=0;
+    // //sadnessS
+    // for(let i = 0; i < sentimentResults.length; i++) {
+    //   sadness+=sentimentResults[i].emotion.sadness;
+    // }
+    // sadness/=sentimentResults.length;
+    // sadness = sadness.toFixed(2);
+     //joy
+     for(let i = 0; i < sentimentResults.length; i++) {
+       joy+=sentimentResults[i].emotion.joy;
+     }
+     joy/=sentimentResults.length;
+     joy = joy.toFixed(2);
+    // //fear
+    // for(let i = 0; i < sentimentResults.length; i++) {
+    //   fear+=sentimentResults[i].emotion.fear;
+    // }
+    // fear/=sentimentResults.length;
+    // fear = fear.toFixed(2);
+    // //disgust
+    // for(let i = 0; i < sentimentResults.length; i++) {
+    //   disgust+=sentimentResults[i].emotion.disgust;
+    // }
+    // disgust/=sentimentResults.length;
+    // disgust = disgust.toFixed(2);
+    // //anger
+    // for(let i = 0; i < sentimentResults.length; i++) {
+    //   anger+=sentimentResults[i].emotion.anger;
+    // }
+    // anger/=sentimentResults.length;
+    // anger = anger.toFixed(2);
+    // var neutral = 1 
+    // neutral = neutral - sadness - joy - fear - disgust - anger;
+
+    console.log("RESULTS");
+    // console.log("sadness: "+sadness);
+    console.log("joy: "+joy);
+    // console.log("fear: "+fear);
+    // console.log("disgust: "+disgust);
+    // console.log("anger: "+anger);
+    // console.log("neutral: "+neutral);    
+    res.json(joy);  
+  }catch(error){
+    console.error(error);
+  }
+});
 
 //Posting data
 /*
@@ -155,27 +152,45 @@ app.post('/api', (request, response) => {
 */
 
 
-app.get('/search', (req, res) => {
+app.get('/searchTwitter', (req, res) => {
     const searchTerm = req.query.term;
-    T.get('search/tweets', { q: searchTerm,count: 20}, function(err, data, response) {
+    T.get('search/tweets', { q: searchTerm,count: 100}, function(err, data, response) {
       if(err) return res.status(500).json({ error: err });
       //res.json(data);
       const tweets = data.statuses;
-      var index = 0;
       var sentiment = new Sentiment();
       var totalScore = 0;
       for (var i = 0; i < tweets.length; i++){
-          index++;
-          console.log("\n",index," ",tweets[i].text);
           const result = sentiment.analyze(tweets[i].text);
           totalScore += result.score;
-          console.log("Postive: {" ,result.positive, "}","Negative: {", result.negative,"}")
-          console.log("score: ",result.score);
+          //console.log("\n",i," ",tweets[i].text);
+          //console.log("Postive: {" ,result.positive, "}","Negative: {", result.negative,"}")
+          //console.log("score: ",result.score);
       }
       //send back the data from the tweets aswell as average sentiment
         const averageSentiment = totalScore/tweets.length;
-        console.log(averageSentiment);
-        res.json(averageSentiment);
+        console.log("AVGS SCORE TWEETS: "+ averageSentiment);
+        if (averageSentiment > 1) 
+        {
+          emoji = "😁";
+        } 
+        else if (averageSentiment < -1) 
+        {
+            emoji = "😡";
+        } 
+        else if (averageSentiment < 0) 
+        {
+            emoji = "😔";
+        } 
+        else if (averageSentiment > 0) 
+        {
+            emoji = "🙂"; 
+        } 
+        else 
+        {
+            emoji = "😐";
+        }
+        res.json(averageSentiment + emoji);
         //res.json({tweets, averageSentiment});
       
     });
@@ -183,7 +198,6 @@ app.get('/search', (req, res) => {
 
 //reccieves text from the client textarea then returns the sentiment of the text to the client page
 
-    
 app.get('/customSentiment', (req, res) => {
     //analzes the textarea text from the client after the user clicks the button
     const text = req.query.term;
